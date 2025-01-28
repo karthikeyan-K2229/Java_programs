0871e1b6fbce8beb7503ca506b4bc09dc3dc304f
karthikeyan.k <k.karthikeyankcbe@gmail.com>
2025-01-28 22:27:37 +0530

    ArrayDSA3

--- src/ArraysDSA/findDuplicates.java
+++ src/ArraysDSA/findDuplicates.java
@@ -1,8 +1,25 @@
 package ArraysDSA;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class findDuplicates {
     public static void main(String[] args) {
         int[] arr= {4,3,2,7,8,2,3,1};
-
+        List<Integer> a=new ArrayList<>();
+        Arrays.sort(arr);
+        //12233478
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]==arr[i-1])
+            {
+                a.add(arr[i-1]);
+            }
+        }
+        for (int i:a
+             ) {
+            System.out.println(i);
+        }
     }
 }
